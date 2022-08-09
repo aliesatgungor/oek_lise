cafb978a20b375867c545437b5a1238e2ddbd4d9
aliesatgungor <aliesatgungor@outlook.com.tr>
2022-08-09 18:16:39 +0300

    sa

--- lib/Pages/9.Sinif/biyoloji.dart
+++ lib/Pages/9.Sinif/biyoloji.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-class Biyoloji9 extends StatefulWidget {
 
+class Biyoloji9 extends StatefulWidget {
   Biyoloji9({Key? key}) : super(key: key);
   @override
   _Biyoloji9State createState() => _Biyoloji9State();
 }
+
 class _Biyoloji9State extends State<Biyoloji9> {
   final GlobalKey webViewKey = GlobalKey();
 
   InAppWebViewController? webViewController;
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
       crossPlatform: InAppWebViewOptions(
-
         useShouldOverrideUrlLoading: true,
         mediaPlaybackRequiresUserGesture: false,
       ),
       android: AndroidInAppWebViewOptions(
-
         useHybridComposition: true,
       ),
       ios: IOSInAppWebViewOptions(
-
         allowsInlineMediaPlayback: true,
       ));
 
@@ -32,15 +30,15 @@ class _Biyoloji9State extends State<Biyoloji9> {
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-        body: InAppWebView(
+    return SafeArea(
+        child: Scaffold(
+      body: InAppWebView(
         key: webViewKey,
-        initialUrlRequest:
-        URLRequest(url: Uri.parse("https://www.dersizleyin.com/kategori/lise/9-sinif/biyoloji-9/")),
+        initialUrlRequest: URLRequest(
+            url: Uri.parse(
+                "https://www.dersizleyin.com/kategori/lise/9-sinif/biyoloji-9/")),
         initialOptions: options,
-
       ),
-    );
+    ));
   }
 }
